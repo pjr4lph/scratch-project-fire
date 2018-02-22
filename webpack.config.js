fdc2ab185c01dafefb02678219b08ff34b571168
Aleksander Turberville <aleksville@live.com>
2018-02-22 17:18:56 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const entry = [
+  './client/app.js'
+];
+
+const output = {
+  path: path.resolve(__dirname, 'build'),
+  publicPath: '/build/',
+  filename: 'bundle.js',
+};
+
+module.exports = {
+  entry, output,
+  devtool: "eval-source-map",
+  module: {
+    loaders: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets:[ 'es2015', 'react' ]
+        }
+      },
+    ],
+  },
+};  
