0fdfb01615eafcddb1f1a66ad57b679eeffa8198
Max Latoche <max.latoche@gmail.com>
2018-02-23 10:34:49 -0500

    adds route for getting all mongo documents, Model for repo documents

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const mongoURI = process.env.NODE_ENV === 'mongodb://teamfire:p%40ssword@ds245518.mlab.com:45518/teamfirescratchproject';
+mongoose.connect(mongoURI);
+
+app.use(bodyParser);
+
+app.get('/' repoController.getAllRepos);
+
+// app.get('/login', (req, res) => {
+//     res.redirect('githubloginurl');
+// });
+//
+// app.post('/login', (req, res) => {
+//
+// });
+
+
+app.listen(8080, 'listening on 3000')
