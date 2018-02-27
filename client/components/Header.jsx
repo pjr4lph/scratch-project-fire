4c93bd5f90a5a925f2ef2d03dfabccc577c6e48e
sarahheacock <seheacock@bellsouth.net>
2018-02-26 21:34:51 -0500

    ghjk

---
+++ client/components/Header.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const Header = (props) => {
+	return (
+		<nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+		  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span className="navbar-toggler-icon"></span>
+		  </button>
+		  <a className="navbar-brand" href="#"><div className="icon"><i className="fa fa-github fa-2x"></i></div><div>Github Open Source</div></a>
+
+		  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul className="navbar-nav mr-auto">
+					{(props.links).map((link) => (
+						<li key={link} className={(props.current === link) ? "nav-item active": "nav-item"}>
+			        <a className="nav-link" name={link} href="#" onClick={(e) => {props.updateCurrent(link)}}>{link}</a>
+			      </li>
+					))}
+		    </ul>
+				<a className="btn btn-outline-primary my-2 my-sm-0" href="https://github.com/login/oauth/authorize?client_id=9d2b207f821e93af85cc&redirect_uri=http://localhost:8081/auth&scope=read%3Auser">Login</a>
+		  </div>
+		</nav>
+
+	);
+}
+
+Header.propTypes = {
+	links: PropTypes.array.isRequired,
+	current: PropTypes.string.isRequired,
+	updateCurrent: PropTypes.func.isRequired
+}
+
+export default Header;
--- client/index.jsx
+++ client/index.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './app.jsx';
+import App from './App.jsx';
 // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { BrowserRouter, Route } from 'react-router-dom'
-import Routes from './Routes.jsx';
 
-import style from './main.scss'
-// render(
-//   <App />, document.getElementById('app')
-//   );
+import style from './scss/main.scss';
 
-render((
-    <App />
-
-), document.getElementById('app'))
+render(<App />, document.getElementById('app'));
