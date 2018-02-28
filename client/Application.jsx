74c087417ce61a44e63bf663adcc6bfa2859dbd4
sarahheacock <seheacock@bellsouth.net>
2018-02-28 14:18:04 -0500

    saving filter

--- client/Application.jsx
+++ client/Application.jsx
@@ -1,13 +1,13 @@
-import Header from './components/header.jsx';
+import Header from './components/Header.jsx';
+import Input from './components/Input.jsx';
 import RepoList from './components/RepoList.jsx'
 import React, { Component } from 'react';
 import ReactDom from "react-dom";
-// import { Grid } from 'react-bootstrap';
-import axios from 'axios';
+
 import Modal from './components/Modal.jsx';
+import { util, fetchUser, fetchRepos } from './utils.js'
 
-
-const links = ['Suggested', 'Popular', 'Difficulty'];
+const links = ['Suggested', 'Most Popular', 'Most Open Issues'];
 
 class Application extends Component {
 	constructor(props) {
@@ -23,26 +23,37 @@ class Application extends Component {
 
   componentDidMount() {
 		const modal = document.getElementById('myModal');
-		 modal.style.display = 'none';
+		modal.style.display = 'none';
     const allCookies = document.cookie;
-    
-		fetch("http://localhost:8081/getRepos")
-    .then(res => {
-			return res.json();
-		})
-    .then(repos => {
-			this.setState({ repos })
-		})
+
+		if(window.location.search){
+			fetchRepos().then((repos) => {
+				return fetchUser(repos);
+			}).then((newState) => {
+				this.setState(newState);
+			})
+		}
+		else {
+			fetchRepos().then((repos) => {
+				this.setState({ repos })
+			})
+		}
   }
 
 	updateCurrent = (name) => {
-		this.setState((prevState) => {	
-			if (name === 'Suggested') {         // && !this.state.user.login 
-				const modal = document.getElementById('myModal');
-				modal.style.display = 'block';
-			}
+		// if user is not logged, do not change order
+		if (name === 'Suggested' && !Object.keys(user).length) {
+			const modal = document.getElementById('myModal');
+			modal.style.display = 'block';
+			return;
+		}
+
+		this.setState((prevState) => {
+			const repos = util(name, prevState.repos, prevState.user, links);
+
 			return {
-				current: name
+				current: name,
+				repos: repos
 			}
 		})
 	}
@@ -53,27 +64,20 @@ class Application extends Component {
 	}
 
   render() {
-		// console.log(this.state.repos)
+		console.log(this.state);
+
     return (
       <div>
 				<Modal modalFunctions={this.modalFunctions}/>
-				<div>
 				<Header
-				user={this.state.user.login}
-				links={links}
-				current={this.state.current}
-				updateCurrent={this.updateCurrent}
+					user={this.state.user.login}
+					links={links}
+					current={this.state.current}
+					updateCurrent={this.updateCurrent}
 				/>
-				</div>
-			
-			<div className="form-container">
-				<input className="form-control" type="text" placeholder="Search Repos"></input>
-				<button className="btn btn-primary btn-search" type="submit">Search <i className="fa fa-search"></i></button>
-			</div>
-			<RepoList repos={this.state.repos} />
-			 {/* Model button */}
-			<button id="myBtn">Open Modal</button>
-		 </div>
+				<Input />
+				<RepoList repos={this.state.repos} />
+			 </div>
 		);
   }
 }
